5ac2583b8495a595cd326f35e826898ce0f899bf
Sahil Shah <158504710+SahilS1903@users.noreply.github.com>
2025-07-05 17:14:39 +0530

    Time: 193 ms (18.22%), Space: 14.1 MB (33.24%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            result ^= num
+            print(result)
+        return result
+
+
+        
