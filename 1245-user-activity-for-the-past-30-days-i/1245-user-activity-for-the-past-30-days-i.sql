58ff8520505088d047a78d17a3d032bb3822e5c9
Sahil Shah <158504710+SahilS1903@users.noreply.github.com>
2025-07-18 23:31:30 +0530

    Time: 434 ms (95.37%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -3,5 +3,5 @@ select activity_date as day,
 count(distinct user_id)as active_users
 from activity
 where 
-activity_date between date_sub('2019-07-27',interval 29 day) and '2019-07-27'
+datediff('2019-07-27 ',activity_date) between 0 and 29
 group by activity_date
