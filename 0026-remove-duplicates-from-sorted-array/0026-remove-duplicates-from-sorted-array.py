e482746085b42e3e4024bd4297794702e68169cc
Sahil Shah <158504710+SahilS1903@users.noreply.github.com>
2025-07-20 01:11:00 +0530

    Time: 5 ms (32.63%), Space: 14 MB (8.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,14 +1,26 @@
+# class Solution(object):
+    # def removeDuplicates(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     new_set=set(nums)
+        
+    #     ans=list(sorted(new_set))
+        
+    #     for i in range(len(ans)):
+    #         nums[i]=ans[i]
+    #     return len(ans)
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        new_set=set(nums)
-        
-        ans=list(sorted(new_set))
-        
-        for i in range(len(ans)):
-            nums[i]=ans[i]
-        return len(ans)
-        
+        i=0
+        for j in range (1,len(nums)):
+            if nums[j]>nums[i]:
+                nums[i+1]=nums[j]
+                i+=1
+        return i+1
