1b115562548c8a96a2000352257950f8ad1f31f8
Sahil Shah <158504710+SahilS1903@users.noreply.github.com>
2025-07-21 02:43:18 +0530

    Time: 3 ms (35.56%), Space: 12.5 MB (26.33%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,17 +1,25 @@
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         for i in range(len(strs[0])):                  # loop through each character index of the first string
+#             key = strs[0][:i+1]                         # take prefix of length (i+1) from the first string
+#             print(key)                                  # print current prefix (for debugging)
+#             for j in range(1, len(strs)):               # loop over the rest of the strings
+#                 if key != strs[j][:i+1]:                # if the prefix doesn’t match in any string
+#                     return strs[0][:i]                  # return the prefix up to index i (excluding current character)
+#         return strs[0]                                  # if full first string is prefix, return it
+
+
+
+
 class Solution(object):
     def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        
-        for i in range(len(strs[0])):
-            key=strs[0][:i+1]
-            print(key)
-            for j in range(1,len(strs)):
-                if key!=strs[j][:i+1]:
-                    return strs[0][:i]
-            
-        return strs[0]
+        if not strs:
+            return ""
 
-        
+        for i in range(len(strs[0])):             # loop over each character index in the first string
+            char = strs[0][i]                     # take the i-th character
+            for s in strs[1:]:                    # check this char against all other strings
+                if i >= len(s) or s[i] != char:   # mismatch or string is shorter
+                    return strs[0][:i]            # return common prefix up to i
+        return strs[0]                            # all characters matched
+
