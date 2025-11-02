152581b21bd67fa35a0a819c8fdc8747de1fe7db
Sahil Shah <158504710+SahilS1903@users.noreply.github.com>
2025-11-02 15:03:44 +0530

    Time: 11 ms (28.99%), Space: 19.4 MB (50.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = 0
-        candidate = None
-
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-
-        return candidate
-
-        
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt=0
+        for i in range(len(nums)):
+            if cnt==0:
+                el=nums[i]
+                cnt+=1
+            elif nums[i]==el:
+                cnt+=1
+            else:
+                cnt-=1
+        return el
